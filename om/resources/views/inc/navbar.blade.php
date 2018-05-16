e1f9fba4d9fc536bb1f875c9e6d71e42f9afbc88
Your Name <you@example.com>
2018-05-16 09:57:48 +0530

    laravel blog

---
+++ om/resources/views/inc/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="../public">{{config('app.name','OM')}}</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+      
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="../public">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../public/about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../public/services">Service</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="../public/posts">Blog</a>
+                </li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="../public/posts/create">Create Post</a></li>
+              </ul>
+              
+            </div>
+          </nav>
